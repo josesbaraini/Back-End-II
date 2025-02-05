0a1aa729794b0c530d8eee5f198bc95fee89c175
José Sbaraini <josesbarainips@gmail.com>
2025-02-05 10:03:04 -0400

    opa

--- ApiPoliclinica/servico/conexao.js
+++ ApiPoliclinica/servico/conexao.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 import 'dotenv/config';
 
 const pool = mysql.createPool({
-    port: process.env.PORT,
+    port: process.env.PORT_BD,
     host     : process.env.HOST,
     user     : process.env.USER,
     password : process.env.PASSWORD,
