bd881ba58d61c0a78a6925a7f7224cd1fc6267c8
josesbaraini <josesbarainips@gmail.com>
2025-02-20 17:42:00 -0400

    se é lco

--- ApiCadastroUsuarios/index.js
+++ ApiCadastroUsuarios/index.js
@@ -1,33 +1,44 @@
 import express from 'express';
 import cors from 'cors';
 import { cadastraLead } from "./servico/cadastro_servico.js";
-import { validaUsuario } from "./validacao/valida.js";
+import { validaUsuario, validaUsuario2 } from "./validacao/valida.js";
 
 const app = express();
 app.use(cors())
 app.use(express.json());
 app.post('/usuarios', async (req, res) => {
-        const {nome,email,telefone} = req.body;
+    const { nome, email, telefone } = req.body;
 
-        if (validaUsuario(nome,email,telefone) === true){
-            await cadastraLead(nome, email, telefone)
-            res.status(204).send('Cadastro completo')
+    if (validaUsuario(nome, email, telefone)) {
+        await cadastraLead(nome, email, telefone)
+        res.status(204).send('Cadastro completo')
 
-        }else{
-            res.status(400).send('Dados de cadastro invalidos')
+    } else {
+        res.status(400).send('Dados de cadastro invalidos')
 
-        }
-        
-        
+    }
+
+
 
 })
 
+app.post('/usuarios2', async (req, res) => {
+    const { nome, email, telefone } = req.body;
+    const usuarioValido = validaUsuario2(nome, email, telefone)
+    if (usuarioValido.status) {
+        await cadastraLead(nome, email, telefone)
+        res.status(204).send('Cadastro Completo')
+
+    } else {
+        res.status(400).send(usuarioValido.mensagem)
+    }
+})
 
 app.listen(9001, () => {
 
     const data = new Date();
     console.log("Servidor node iniciado em: " + data);
-    
+
 
 })
 
--- ApiCadastroUsuarios/validacao/valida.js
+++ ApiCadastroUsuarios/validacao/valida.js
@@ -1,4 +1,36 @@
 
+function validaNome(nome) {
+    const regexNome = /^[a-zA-ZÁ-ÿ\s\-']+$/;
+    const isvalid= regexNome.test(nome) && nome.lenght >=2
+    return isvalid
+}
+function validaEmail(email) {
+    const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isvalid = regexEmail.test(email)
+    return isvalid
+}
+
+function validaTelefone(telefone) {
+    const regexTelefone = /^\(\d{2}\) \d{5}-\d{4}$/;
+    const isvalid = regexTelefone.test(telefone)
+    return isvalid
+}
+
+export function validaUsuario2(nome, email, telefone) {
+    const nomeValido = validaNome(nome);
+    const emailValido =  validaEmail(email);
+    const telefoneValido = validaTelefone(telefone);
+    const usuarioValido = nomeValido && emailValido && telefoneValido
+
+    if (usuarioValido)  {
+        return {status: true, mensagem:''}
+
+        
+    }else{
+        return {status: false, mensagem:'Nome e/ou E-mail invalido(s).'}
+    }
+}
+
 export function validaUsuario(nome, email, telefone) {
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
